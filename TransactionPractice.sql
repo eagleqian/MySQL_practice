ddf59f986b9831114e57c7b7e986ec5389415024
eagleqian <eagleqian321@gmail.com>
2021-08-01 23:37:37 +0800

    Revert "Revert "Revert "Revert "测试回滚""""
    This reverts commit 7cc91f86eda15bae9ce578d6bc9959f49518f5cc.

--- TransactionPractice.sql
+++ TransactionPractice.sql
@@ -1,9 +1,9 @@
-CREATE TABLE transfer(
-`id` INT(5) NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(20) NOT NULL,
-`money` DECIMAL(8,2) NOT NULL,
-PRIMARY KEY (`id`)
-);
+CREATE TABLE transfer (
+    `id` INT(5) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `money` DECIMAL(8 , 2 ) NOT NULL,
+    PRIMARY KEY (`id`)
+)  ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
 -- insert the values 
 INSERT INTO transfer ( `name`, `money`) VALUES ('eagle', '2000.00'),('tiger','10000.00');
@@ -14,8 +14,34 @@ INSERT INTO transfer ( `name`, `money`) VALUES ('eagle', '2000.00'),('tiger','10
 SET autocommit = 0;
 
 -- START transaction
-START TRANSACTION  -- open a transaction
+START TRANSACTION;-- open a transaction
 
-UPDATE 
+UPDATE transfer 
+SET 
+    money = money - 500
+WHERE
+    id = 1;
+    
+UPDATE transfer 
+SET 
+    money = money + 500
+WHERE
+    id = 2;
 
+-- COMMIT transaciton IF success  
+COMMIT; -- once the THRANSACTION is committed, the data persistence is finished
 
+-- ROLLBACK transaction IF fail
+ROLLBACK;
+
+-- CLOSE autocommit
+SET autocommit = 1;
+
+-- show the table
+SELECT * FROM business.transfer;
+
+-- show the state of autocommit
+show session variables like 'autocommit';
+
+-- drop the table
+DROP TABLE transfer;
